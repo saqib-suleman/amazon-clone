ab65bb2c6f0da07aa64297c10721db24b6f2c123
Saqib <saqibs3291@gmail.com>
2022-11-01 20:51:52 +0500

    added header component

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: 'AIzaSyASh4ouUaxcljHSJbqIwAOw6ttrHD7dunI',
+  authDomain: 'clone-374e4.firebaseapp.com',
+  projectId: 'clone-374e4',
+  storageBucket: 'clone-374e4.appspot.com',
+  messagingSenderId: '782168734603',
+  appId: '1:782168734603:web:179f8eee9eb51cf873cd8e',
+};
